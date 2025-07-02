f406cc1af20fb10bbae78bc6021e99db9535ebe0
vlad <vladsheverda7@gmail.com>
2025-07-02 22:03:33 +0400

    Complete find unique value in array

--- tasks_for_the_interview/find_unique_value_in_array.ts
+++ tasks_for_the_interview/find_unique_value_in_array.ts
@@ -1,12 +1,9 @@
 export function findUniq(arr: number[]): number {
     let uniqueValue = 0;
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] != arr[j]) {
-                uniqueValue = arr[i];
-            } else {
-                break;
-            }
+    let newArr = arr.sort();
+    for (let i = 0; i < newArr.length; i++) {
+        if (newArr[i] !== newArr[i - 1] && newArr[i] !== newArr[i + 1]) {
+            uniqueValue = newArr[i];
         }
     }
     return uniqueValue;
