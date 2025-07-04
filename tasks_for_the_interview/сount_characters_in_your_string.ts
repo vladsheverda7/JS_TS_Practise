0d8bb14cefdb871e9041e394cff072b313a24981
vlad <vladsheverda7@gmail.com>
2025-07-04 12:28:44 +0400

    completed count chars in the string

--- tasks_for_the_interview/сount_characters_in_your_string.ts
+++ tasks_for_the_interview/сount_characters_in_your_string.ts
@@ -2,12 +2,10 @@ export function count(string: string): object {
     const returnedObject: { [key: string]: number } = {};
     const arrFromString = string.split('');
     const uniqueArr = [...new Set(arrFromString)];
-    let charCounter = 1;
 
     for (let i = 0; i < uniqueArr.length; i++) {
-        console.log(`arr[i] = ${uniqueArr[i]}`);
+        let charCounter = 0;
         for (let j = 0; j < arrFromString.length; j++) {
-            console.log(`arr[j] = ${arrFromString[j]}`);
             if (uniqueArr[i] === arrFromString[j]) {
                 charCounter++;
             }
